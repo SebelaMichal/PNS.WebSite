62c62cf8ed51bfe3872c6975df4e9c72fe928b30
Michal Šebela <sebelamichal@hotmail.com>
2024-05-15 21:30:36 +0200

    první commit

---
+++ source/ProgramatoriNaStream.WebSite/ProgramatoriNaStream.WebSite/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using ProgramatoriNaStream.WebSite.Data;
+
+namespace ProgramatoriNaStream.WebSite.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
